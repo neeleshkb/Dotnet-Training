8850eebfe409898b04d327f64dae27e86d97937e
neeleshkb <bodgalneelesh@gmail.com>
2025-11-28 10:06:33 +0530

    Added Sql scripts

---
+++ Adonet.Sessions/scripts/Learning.DDL.sql
@@ -0,0 +1,32 @@
+use Learning;
+
+Create Table Students(
+	StudentId int primary key identity(1,1),
+	FirstName nvarchar(50), -- nvarchar supports Unicode characters
+	LastName nvarchar(50),
+	Dob date, -- YYYY-MM-DD
+	Active bit -- 0 or 1
+)
+
+Create table Enrollments
+(
+	EnrollmentId int primary key identity(1,1),
+	StudentId int not null,
+	CourseId int not null,
+	Grade nchar(1) not null
+)
+
+Create table Courses
+(
+	CourseId int primary key identity(1,1),
+	CourseName nvarchar(100),
+	Credits int not null
+)
+
+-- Updating existing column of existing table.
+Alter table Students Alter Column FirstName nvarchar(50) Not Null
+Alter table Students Alter Column Dob date Not Null
+
+-- Adding a new colum to existing table
+Alter table Students Add Semseter int not null
+
---
+++ Adonet.Sessions/scripts/Learning.DML.sql
@@ -0,0 +1,25 @@
+use Learning;
+
+Select * from Students;
+Select * from Courses
+Select * from enrollments
+
+--insert into Students (FirstName, LastName, Dob, Active, Semseter) values ('John', 'liver', '2000-12-01', 1, 1);
+--insert into Students (FirstName, LastName, Dob, Active, Semseter) values ('Bob', null, '2000-11-01', 1, 3);
+--insert into Students (FirstName, LastName, Dob, Active, Semseter) values ('Alice', null, '2000-10-01', 1, 7);
+
+--insert into Courses (CourseName, Credits) Values ('Biology',9);
+--insert into Courses (CourseName, Credits) Values ('Maths',8);
+--insert into Courses (CourseName, Credits) Values ('Chemistry',6);
+
+-- A student can enroll in multiple courses
+-- A course can be enrolled by multiple students
+-- Many to Many relationship
+-- Grades A,B,C,E
+
+insert into enrollments (studentid, courseid, grade) values (1,1,'A');
+insert into enrollments (studentid, courseid, grade) values (1,2,'B');
+insert into enrollments (studentid, courseid, grade) values (1,3,'E');
+
+insert into enrollments (studentid, courseid, grade) values (2,1,'A');
+insert into enrollments (studentid, courseid, grade) values (2,2,'A');
